fa528403b3a223a929510008e3210be4d9edb074
David Abdurachmanov <davidlt@cern.ch>
2013-10-20 06:17:59 -0400

    build_rpm.sh: correct location for config.{guess,sub} for NSPR
    Signed-off-by: David Abdurachmanov <davidlt@cern.ch>

--- build_rpm.sh
+++ build_rpm.sh
@@ -164,8 +164,8 @@ make -j $BUILDPROCESSES && make install
 cd $RPM_SOURCES/nspr-4.10.1/nspr
 
 # Update for AAarch64
-rm -f ./nspr/build/autoconf/config.sub && curl -L -k -s -o ./nspr/build/autoconf/config.sub 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
-rm -f ./nspr/build/autoconf/config.guess && curl -L -k -s -o ./nspr/build/autoconf/config.guess 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
+rm -f ./build/autoconf/config.sub && curl -L -k -s -o ./build/autoconf/config.sub 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+rm -f ./build/autoconf/config.guess && curl -L -k -s -o ./build/autoconf/config.guess 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
 
 ./configure --host="${CONFIG_HOST}" --build="${CONFIG_BUILD}" --disable-rpath \
             --prefix $PREFIX $NSPR_CONFIGURE_OPTS
